1607fc4fc6dbfe2939fb0b16ab274dec3e7cf8cd
Evg <evg.tray@gmail.com>
2016-12-24 15:19:28 +0300

    Improve models

--- db/migrate/20161222162345_create_questions.rb
+++ db/migrate/20161222162345_create_questions.rb
@@ -1,8 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
 
       t.timestamps
     end
--- db/migrate/20161222162655_create_answers.rb
+++ db/migrate/20161222162655_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
-      t.text :body
+      t.text :body, null: false
       t.references :question, foreign_key: true, index: true
 
       t.timestamps
