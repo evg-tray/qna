5878366925e92ab2ac9ac82e7f9446ba2c9b4865
Evg <evg.tray@gmail.com>
2017-01-20 16:42:25 +0300

    Refactor

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,7 +1,6 @@
 module AnswersHelper
   def link_to_delete_answer(question, answer)
-    return unless user_signed_in?
-    return unless current_user.author_of?(answer)
+    return unless user_signed_in? && current_user.author_of?(answer)
     link_to 'Delete answer', question_answer_path(question, answer), method: :delete
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,6 @@
 module QuestionsHelper
   def link_to_delete_question(question)
-    return unless user_signed_in?
-    return unless current_user.author_of?(question)
+    return unless user_signed_in? && current_user.author_of?(question)
     link_to 'Delete question', question_path(question), method: :delete
   end
 end
