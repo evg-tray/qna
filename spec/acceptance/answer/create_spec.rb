cd06f62fee9b7a66a21fe14d8f5d89c2adba29e5
Evg <evg.tray@gmail.com>
2017-01-20 18:07:02 +0300

    Add ajax create answer

--- spec/acceptance/answer/create_spec.rb
+++ spec/acceptance/answer/create_spec.rb
@@ -7,26 +7,27 @@ feature 'Create answer', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  scenario 'Authenticated user creates answer' do
+  scenario 'Authenticated user creates answer', js: true do
     sign_in(user)
     visit question_path(question)
     answer_text = Faker::Lorem.characters(50)
     fill_in 'Body', with: answer_text
     click_on 'Create answer'
 
-    expect(page).to have_content 'Your answer successfully created.'
     expect(page).to have_content question.title
     expect(page).to have_content question.body
-    expect(page).to have_content answer_text
+    within '.answers' do
+      expect(page).to have_content answer_text
+    end
   end
 
-  scenario 'Authenticated user tries create answer with invalid body' do
+  scenario 'Authenticated user tries create answer with invalid body', js: true do
     sign_in(user)
     visit question_path(question)
-    fill_in 'Body', with: ''
+    fill_in 'Body', with: '333'
     click_on 'Create answer'
 
-    expect(page).to have_content 'Your answer is invalid.'
+    expect(page).not_to have_content '333'
 
   end
 
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
   describe 'POST #create' do
     sign_in_user
-    let!(:create_post_params) { {answer: attributes_for(:answer), question_id: question} }
-    let!(:create_post_invalid_params) { {answer: attributes_for(:answer, :invalid_body), question_id: question} }
+    let!(:create_post_params) { {answer: attributes_for(:answer), question_id: question, format: :js} }
+    let!(:create_post_invalid_params) { {answer: attributes_for(:answer, :invalid_body), question_id: question, format: :js} }
     let!(:create_post) do
       Proc.new do |params = create_post_params|
         post :create, params: params
@@ -15,9 +15,9 @@ RSpec.describe AnswersController, type: :controller do
         expect { create_post.call }.to change(Answer, :count).by(1)
       end
 
-      it 'redirect to show view with question route' do
+      it 'render create template' do
         create_post.call
-        expect(response).to redirect_to question_path(assigns(:question))
+        expect(response).to render_template :create
       end
     end
 
@@ -26,9 +26,9 @@ RSpec.describe AnswersController, type: :controller do
         expect { create_post.call(create_post_invalid_params) }.to_not change(Answer, :count)
       end
 
-      it 'redirect to show view with question route' do
+      it 'render create template' do
         create_post.call(create_post_invalid_params)
-        expect(response).to redirect_to question_path(assigns(:question))
+        expect(response).to render_template :create
       end
     end
   end
